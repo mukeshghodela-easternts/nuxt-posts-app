9bcafe6b44cbcc1eb22a54a58c8f32e5e6a43b8a
Mukesh Prajapati <mukesh.easternts@gmail.com>
2020-12-30 09:28:26 +0530

    - Initial Commit -> Created about + posts pages

---
+++ pages/about/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about-section">
+    <h1>About Us Page</h1>
+    <p>Some text about who we are and what we do.</p>
+    <p>Resize the browser window to see that this page is responsive by the way.</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "index.vue"
+}
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+.about-section  h1, .about-section  p{
+  padding-bottom: 20px;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue"
+}
+</script>
+
+<style scoped>
+.posts-page {
+  /*display: flex;*/
+  /*justify-content: center;*/
+  /*align-items: center;*/
+}
+</style>
